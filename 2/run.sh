8e2ab1c31b02247664c9fe7563b65d9178029fe2
root <serhat94@outlook.com>
2021-04-30 22:38:20 +0800

    consul done

---
+++ 2/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+kubectl --insecure-skip-tls-verify apply -f consul-gossip-encryption-key.yml
+helm repo add hashicorp https://helm.releases.hashicorp.com
+helm upgrade --install consul hashicorp/consul -f consul-helm-values.yml --kubeconfig=$HOME/admin.conf --version 0.20.1
