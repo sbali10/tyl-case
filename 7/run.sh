cfea7a174c2e1fdbd7990018faba5673eadb5256
root <root@node1.case-serhat-bali.abc>
2021-05-03 21:07:37 +0800

    updated

--- 7/run.sh
+++ 7/run.sh
@@ -4,4 +4,4 @@ set -e
 kubectl create ns gitlab
 kubectl --insecure-skip-tls-verify apply -f external-gitlab-svc.yml
 helm repo add gitlab https://charts.gitlab.io
-helm upgrade --install gitlab-runner gitlab/gitlab-runner -f gitlab-runner-helm-values.yml -n gitlab --kubeconfig=/home/kerem/kubeconfig.proxy --version 0.18.0
+helm upgrade --install gitlab-runner gitlab/gitlab-runner -f gitlab-runner-helm-values.yml -n gitlab --version 0.18.0
